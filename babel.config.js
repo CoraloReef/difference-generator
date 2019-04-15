1ea11e1a4860eeac831680ec8503e7a75bce99bf
Viacheslav Gordeiko <gordeikovs@gmail.com>
2019-04-16 00:22:46 +0300

    update README

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
-export const presets = [
-  ['@babel/preset-env',
-    {
-      targets: {
-        node: 'current',
+module.exports = {
+  presets: [
+    ['@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
       },
-    },
+    ],
   ],
-];
+};
