d224583c37647ec1a2a5e7cbde691c74f2ea1571
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-04-18 18:48:04 +0300

    add build AST and rendering

--- src/index.js
+++ src/index.js
@@ -8,30 +8,46 @@ const getObjectPathFile = (pathFile) => {
   return parsers(path.extname(pathFile), file);
 };
 
-const genDiff = (firstPathFile, secondPathFile) => {
-  const objFirstFile = getObjectPathFile(firstPathFile);
-  const objSecondFile = getObjectPathFile(secondPathFile);
-
+const getNod = (objFirstFile, objSecondFile) => {
   const keysFiles = [
     ...Object.keys(objFirstFile),
     ...Object.keys(objSecondFile)
       .filter(key => !_.has(key, objFirstFile)),
   ];
 
-  const difference = keysFiles.reduce((acc, key) => {
+  return keysFiles.reduce((acc, key) => {
     if (!_.has(key, objFirstFile) && _.has(key, objSecondFile)) {
-      return `${acc}  + ${key}: ${objSecondFile[key]}\n`;
+      return { ...acc, [key]: { value: objSecondFile[key], status: 'added' } };
     }
     if (_.has(key, objFirstFile) && !_.has(key, objSecondFile)) {
-      return `${acc}  - ${key}: ${objFirstFile[key]}\n`;
+      return { ...acc, [key]: { value: objFirstFile[key], status: 'removed' } };
     }
     if (objFirstFile[key] === objSecondFile[key]) {
-      return `${acc}    ${key}: ${objFirstFile[key]}\n`;
+      return { ...acc, [key]: { value: objFirstFile[key], status: 'notChanged' } };
     }
-    return `${acc}  + ${key}: ${objSecondFile[key]}\n  - ${key}: ${objFirstFile[key]}\n`;
-  }, '');
+    return { ...acc, [key]: { value: [objFirstFile[key], objSecondFile[key]], status: 'changed' } };
+  }, {});
+};
 
-  return `{\n${difference}}`;
+const render = (ast) => {
+  const result = _.keys(ast).reduce((acc, key) => {
+    const item = {
+      added: `+ ${key}: ${ast[key].value}`,
+      removed: `- ${key}: ${ast[key].value}`,
+      notChanged: `  ${key}: ${ast[key].value}`,
+      changed: `+ ${key}: ${ast[key].value[1]}\n  - ${key}: ${ast[key].value[0]}`,
+    };
+    return `${acc}  ${(item[ast[key].status])}\n`;
+  }, '');
+  return `{\n${result}}`;
+};
+
+const genDiff = (firstPathFile, secondPathFile) => {
+  const objFirstFile = getObjectPathFile(firstPathFile);
+  const objSecondFile = getObjectPathFile(secondPathFile);
+
+  const ast = getNod(objFirstFile, objSecondFile);
+  return render(ast);
 };
 
 export default genDiff;
