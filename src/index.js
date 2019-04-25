f19e0db913e250c7c9783dd80c3aeed4e409f45a
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-04-26 00:07:24 +0300

    add support tree finish

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const getIndent = depth => '  '.repeat(depth);
 
 const stringify = (value, depth) => {
   if (typeof value === 'object') {
-    const lines = Object.keys(value).map((key) => `${getIndent(depth + 4)}${key}: ${value[key]}\n`);
+    const lines = Object.keys(value).map(key => `${getIndent(depth + 4)}${key}: ${value[key]}\n`);
     return `{\n${lines}${getIndent(depth + 2)}}`;
   }
   return value;
@@ -23,7 +23,7 @@ const getAst = (objFirstFile, objSecondFile) => {
     ...Object.keys(objFirstFile),
     ...Object.keys(objSecondFile)
       .filter(key => !_.has(key, objFirstFile)),
-  ];
+  ].sort();
 
   return keysFiles.reduce((acc, key) => {
     const valueFirst = objFirstFile[key];
@@ -47,23 +47,24 @@ const getAst = (objFirstFile, objSecondFile) => {
 };
 
 const render = (ast) => {
-  const getLines = (nodes, depth = 1) => _.keys(nodes).map((key) => {
+  const getLines = (nodes, depth = 0) => _.keys(nodes).map((key) => {
     const { children, value, status } = nodes[key];
 
     if (children !== undefined) {
-      return `${getIndent(depth + 1)}${key}: {\n${_.flatten(getLines(children, depth + 1)).join('\n')}\n${getIndent(depth + 1)}}`;
+      return `${getIndent(depth + 2)}${key}: {\n${_.flatten(getLines(children, depth + 2)).join('\n')}\n${getIndent(depth + 2)}}`;
     }
 
     const line = {
       added: `+ ${key}: ${stringify(value, depth)}`,
       removed: `- ${key}: ${stringify(value, depth)}`,
       notChanged: `  ${key}: ${stringify(value, depth)}`,
-      changed: [`+ ${key}: ${stringify(value.new, depth)}`, `- ${key}: ${stringify(value.old, depth)}`],
+      changed: [`- ${key}: ${stringify(value.old, depth)}`, `+ ${key}: ${stringify(value.new, depth)}`]
+        .join(`\n${getIndent(depth + 1)}`),
     };
     return `${getIndent(depth + 1)}${line[status]}`;
   });
 
-  const lines = (_.flatten(getLines(ast))).join('\n');
+  const lines = _.flatten(getLines(ast)).join('\n');
   return `{\n${lines}\n}`;
 };
 
