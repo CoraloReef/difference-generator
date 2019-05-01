3fb6374dfc12eea0c5702d069576a84ee8c89024
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-05-01 21:53:38 +0300

    refactoring cascade formatter

--- src/formatters/cascade.js
+++ src/formatters/cascade.js
@@ -1,25 +1,26 @@
+import _ from 'lodash/fp';
+
 const getIndent = depth => '  '.repeat(depth);
 
 const stringify = (value, depth) => {
   if (typeof value !== 'object') {
     return value;
   }
-  const lines = Object.keys(value).map(key => `${getIndent(depth + 4)}${key}: ${value[key]}\n`);
-  return `{\n${lines}${getIndent(depth + 2)}}`;
+  return `{\n${Object.keys(value).map(key => `${getIndent(depth + 3)}${key}: ${value[key]}`)}\n${getIndent(depth + 1)}}`;
 };
 
 const lineStatus = {
-  added: ((key, depth, value) => `+ ${key}: ${stringify(value, depth)}`),
-  removed: ((key, depth, value) => `- ${key}: ${stringify(value, depth)}`),
-  notChanged: ((key, depth, value) => `  ${key}: ${stringify(value, depth)}`),
+  added: ((key, depth, value) => `${getIndent(depth)}+ ${key}: ${stringify(value, depth)}`),
+  removed: ((key, depth, value) => `${getIndent(depth)}- ${key}: ${stringify(value, depth)}`),
+  notChanged: ((key, depth, value) => `${getIndent(depth)}  ${key}: ${stringify(value, depth)}`),
   changed: ((key, depth, value, valueOld, valueNew) => [
-    `- ${key}: ${stringify(valueOld, depth)}`,
-    `+ ${key}: ${stringify(valueNew, depth)}`,
-  ].join(`\n${getIndent(depth + 1)}`)),
-  parent: ((key, depth, value, valueOld, valueNew, children, getLines) => `${getIndent(1)}${key}: {\n${getLines(children, depth + 2)}\n${getIndent(depth + 2)}}`),
+    `${getIndent(depth)}- ${key}: ${stringify(valueOld, depth)}`,
+    `${getIndent(depth)}+ ${key}: ${stringify(valueNew, depth)}`,
+  ]),
+  parent: ((key, depth, value, valueOld, valueNew, children, getLines) => `${getIndent(depth + 1)}${key}: ${getLines(children, depth + 1)}`),
 };
 
-const getLines = (nodes, depth = 0) => Object.keys(nodes).map((item) => {
+const repareLine = (item, depth, getLines) => {
   const {
     key,
     children,
@@ -27,12 +28,14 @@ const getLines = (nodes, depth = 0) => Object.keys(nodes).map((item) => {
     valueOld,
     valueNew,
     status,
-  } = nodes[item];
+  } = item;
 
-  return `${getIndent(depth + 1)}${lineStatus[status](key, depth, value, valueOld, valueNew, children, getLines)}`;
-}).join('\n');
-
-export default (ast) => {
-  const lines = getLines(ast);
-  return `{\n${lines}\n}`;
+  return lineStatus[status](key, depth, value, valueOld, valueNew, children, getLines);
 };
+
+const getLines = (ast, depth = 0) => {
+  const lines = _.flatten(ast.map(item => repareLine(item, depth + 1, getLines)));
+  return ['{', ...lines, `${getIndent(depth)}}`].join('\n');
+};
+
+export default getLines;
